bba2d8a821a771f0e74bc7c1c974d901336b6c71
Patrick Pecegueiro <pcp@prorius.com.br>
2020-08-27 16:32:11 -0300

    primeiro commit

---
+++ src/app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const cors = require("cors");
+const { uuid } = require('uuidv4');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (req, res) => {
+
+  const { title } = req.query;
+  const results = title ? repositories.filter(repository => repository.title.includes(title))
+  : repositories
+  return res.json(results)
+});
+
+app.post("/repositories", (req, res) => {
+  const { title, url, techs=[] } = req.body;
+  const repository = {id: uuid(), title, url, techs, likes: 0};
+
+  repositories.push(repository);
+  return res.json(repository);
+});
+
+app.put("/repositories/:id", (req, res) => {
+
+
+  const { id } = req.params;   
+  const { title, url, techs=[] } = req.body;
+  const repositoryIndex = repositories.findIndex(repository => repository.id===id);
+  if(repositoryIndex<0) {
+      return res.status(400).json({error: 'REPOSITORY_NOT_FOUND'});
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes:repositories[repositoryIndex].likes
+  }
+
+  repositories[repositoryIndex]=repository;
+  return res.json(repository);
+
+});
+
+app.delete("/repositories/:id", (req, res) => {
+
+  const { id } = req.params; 
+  const repositoryIndex = repositories.findIndex(repository => repository.id===id); 
+  if(repositoryIndex<0) {
+      return res.status(400).json({error: 'REPOSITORY_NOT_FOUND'});
+  }
+  repositories.splice(repositoryIndex,1);
+  return res.status(204).send();
+});
+
+app.post("/repositories/:id/like", (req, res) => {
+  const { id } = req.params;   
+  const repositoryIndex = repositories.findIndex(repository => repository.id===id);
+  if(repositoryIndex<0) {
+      return res.status(400).json({error: 'REPOSITORY_NOT_FOUND'});
+  }
+  repositories[repositoryIndex].likes +=1;
+  return res.json(repositories[repositoryIndex]);
+});
+
+module.exports = app;
